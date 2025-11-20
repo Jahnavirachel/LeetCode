dd14d8c8543a7a3400dcec9932b3734ec737dedf
Jahnavi Rachel <75930764+Jahnavirachel@users.noreply.github.com>
2025-11-20 22:23:02 +0530

    Time: 0 ms (100%), Space: 12.5 MB (50.16%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -5,10 +5,20 @@ class Solution(object):
         :rtype: int
         """
         
+        '''
+        # for two digit numbers
         while num > 9:
             n = num % 10
             m = num // 10
             num = n + m
         
-        return num
+        return num'''
 
+        # for any digit using mathamatical formula
+        
+        if num > 0:
+            result = 1 + ((num - 1) % 9)
+        else:
+            return num
+            
+        return result
